ede4e5fb4a21a376d4f8c56c8f0cd5e5b3edd87c
Qing Yin <qingbyin@gmail.com>
2023-03-19 15:55:44 +0800

    Fix fcitx5

--- home.nix
+++ home.nix
@@ -11,7 +11,7 @@
     # Split up configuration and import pieces of it here:
     # ./modules/emacs/emacs.nix
     ./modules/i3/i3-home.nix
-    ./modules/fcitx5/fcitx5.nix
+    ./modules/fcitx5/fcitx5-home.nix
   ];
 
   # Install packages with plugins and configs
---
+++ modules/fcitx5/fcitx5-home.nix
@@ -0,0 +1,35 @@
+{ config, pkgs, ... }:
+
+{
+  # home manager fcitxt5.nix will do:
+  # 1. Add env like GLFW_IM_MODULE will be auto added by fcitx5.nix
+  # 2. Run fcitx5-daemon
+  # i18n.inputMethod = {
+  #   enabled = "fcitx5";
+  #   fcitx5.addons = with pkgs; [
+  #     fcitx5-rime
+  #     fcitx5-chinese-addons
+  #     fcitx5-table-extra
+  #     fcitx5-gtk
+  #   ];
+  # };
+
+
+  xdg.configFile."fcitx5/profile".force = true;
+  xdg.configFile."fcitx5/profile".source = ./profile;
+  xdg.configFile."fcitx5/config".force = true;
+  xdg.configFile."fcitx5/config".source = ./config;
+  # xdg.configFile."fcitx5/conf/pinyin.conf".force = true;
+  # xdg.configFile."fcitx5/conf/pinyin.conf".source = ./pinyin.conf;
+  # home.file.".pam_environment".source = ./pam_environment;
+
+  # Theme
+  xdg.configFile."fcitx5/conf/classicui.conf".force = true;
+  xdg.configFile."fcitx5/conf/classicui.conf".source = ./classicui.conf;
+  home.file.".local/share/fcitx5/themes/Nord/theme.conf".source = ./theme.conf;
+
+  # rime
+  home.file.".local/share/fcitx5/rime/default.custom.yaml".source = ./default.custom.yaml;
+  home.file.".local/share/fcitx5/rime/double_pinyin_flypy.custom.yaml".source = ./double_pinyin_flypy.custom.yaml;
+
+}
