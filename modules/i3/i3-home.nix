778a1a7fd2b037dd447da3e4ba41c0f6e31a7278
Qing Yin <qingbyin@gmail.com>
2023-03-24 23:19:39 +0800

    Fix i3 config

--- modules/i3/i3-home.nix
+++ modules/i3/i3-home.nix
@@ -21,21 +21,21 @@
       xautolock
       rofi
       lxappearance # Modify icon and themes
+      nitrogen # wallpaper manager
   ];
-
 
   home.file.".background-image".source = ../common/watercolor-grunge.jpg;
   xdg.configFile."i3status-rust/config.toml".source = ./i3status.toml;
   # rofi config
   xdg.configFile."rofi/android_notification.rasi".source = ./rofi-theme.rasi;
-  xdg.configFile."rofi/config".text = ''
+  xdg.configFile."rofi/config.rasi".text = ''
   configuration {
       kb-row-up: "Up,Control+k,Control+p";
       kb-row-down: "Down,Control+j,Control+n";
       kb-remove-to-eol: "";
       kb-accept-entry: "Return,KP_Enter";
   }
-  @theme ".config/rofi/android_notification.rasi"
+  @theme "android_notification.rasi"
   '';
 
   # Notification daemon
