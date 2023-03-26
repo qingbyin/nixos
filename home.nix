10721428a25c6c90bf18694174c3329e74f60f39
Qing Yin <qingbyin@gmail.com>
2023-03-26 22:56:02 +0800

    Update chrom extensions

--- home.nix
+++ home.nix
@@ -134,14 +134,22 @@ in
     enable = true;
     package = pkgs.brave; 
     extensions = [
-      "padekgcemlokbadohgkifijomclgjgif" # SwitchyOmega
-      "dndlcbaomdoggooaficldplkcmkfpgff" # New Tab, New Window
-      "dhdgffkkebhmkfjojejmpbldmpobfkfo" # Tampemonkey
-      "dbepggeogbaibhgnhhndojpepiihcmeb" # Vimium
-      "jfedfbgedapdagkghmgibemcoggfppbb" # 猫抓
-      "kkkjlfejijcjgjllecmnejhogpbcigdc" # Org Capture
-      "fkeaekngjflipcockcnpobkpbbfbhmdn" # Copy as Markdown
-      "gmmnidkpkgiohfdoenhpghbilmeeagjj" # Sci-Hub X Now!
+      # SwitchyOmega
+      { id = "padekgcemlokbadohgkifijomclgjgif"; }
+        # New Tab, New Window
+      { id = "dndlcbaomdoggooaficldplkcmkfpgff"; }
+      # Tampemonkey
+      { id = "dhdgffkkebhmkfjojejmpbldmpobfkfo"; }
+        # Vimium
+      { id = "dbepggeogbaibhgnhhndojpepiihcmeb"; }
+      # 猫抓
+      { id = "jfedfbgedapdagkghmgibemcoggfppbb"; }
+      # Org Capture
+      { id = "kkkjlfejijcjgjllecmnejhogpbcigdc"; }
+      # Copy as Markdown
+      { id = "fkeaekngjflipcockcnpobkpbbfbhmdn"; }
+      # Sci-Hub X Now!
+      { id = "gmmnidkpkgiohfdoenhpghbilmeeagjj"; }
     ];
   };
 
