a34a1b79b0f16334d97020096153767ce0345b59
Qing Yin <qingbyin@gmail.com>
2023-04-10 22:52:25 +0800

    Add chrome extensions

--- modules/chrome/chrome.nix
+++ modules/chrome/chrome.nix
@@ -10,20 +10,41 @@
         crxPath = "/home/qyin/.config/chrome/extensions/switchy_omega.crx";
         version = "2.5.21";
       }
-        # New Tab, New Window
-      { id = "dndlcbaomdoggooaficldplkcmkfpgff"; }
+      # New Tab, New Window
+      { id = "dndlcbaomdoggooaficldplkcmkfpgff";
+        crxPath = "/home/qyin/.config/chrome/extensions/new_tab.crx";
+        version = "3.4";
+      }
       # Tampemonkey
-      { id = "dhdgffkkebhmkfjojejmpbldmpobfkfo"; }
-        # Vimium
-      { id = "dbepggeogbaibhgnhhndojpepiihcmeb"; }
+      { id = "dhdgffkkebhmkfjojejmpbldmpobfkfo"; 
+        crxPath = "/home/qyin/.config/chrome/extensions/tampemonkey.crx";
+        version = "4.18.1";
+      }
+      # Vimium
+      { id = "dbepggeogbaibhgnhhndojpepiihcmeb";
+        crxPath = "/home/qyin/.config/chrome/extensions/vimium.crx";
+        version = "1.67.4";
+      }
       # 猫抓
-      { id = "jfedfbgedapdagkghmgibemcoggfppbb"; }
+      { id = "jfedfbgedapdagkghmgibemcoggfppbb";
+        crxPath = "/home/qyin/.config/chrome/extensions/cat_capture.crx";
+        version = "2.3.1";
+      }
       # Org Capture
-      { id = "kkkjlfejijcjgjllecmnejhogpbcigdc"; }
+      { id = "kkkjlfejijcjgjllecmnejhogpbcigdc";
+        crxPath = "/home/qyin/.config/chrome/extensions/org_capture.crx";
+        version = "0.2.1";
+      }
       # Copy as Markdown
-      { id = "fkeaekngjflipcockcnpobkpbbfbhmdn"; }
+      { id = "fkeaekngjflipcockcnpobkpbbfbhmdn";
+        crxPath = "/home/qyin/.config/chrome/extensions/markdown_link.crx";
+        version = "2.7.1";
+      }
       # Sci-Hub X Now!
-      { id = "gmmnidkpkgiohfdoenhpghbilmeeagjj"; }
+      { id = "gmmnidkpkgiohfdoenhpghbilmeeagjj";
+        crxPath = "/home/qyin/.config/chrome/extensions/scihub.crx";
+        version = "0.2.2";
+      }
     ];
   };
   xdg.configFile."chrome/extensions" = {
