d4067bf0d09638f71bf93acb029a81ac0ec2bc74
Qing Yin <qingbyin@gmail.com>
2023-04-17 22:11:58 +0800

    Fix kitty config

--- modules/kitty/kitty.nix
+++ modules/kitty/kitty.nix
@@ -4,6 +4,6 @@
   # Do not install kitty using program.kitty because it needs also install OpenGL
   # Install kitty using Ubuntu, so that kitty can use the system OpenGl
   
-  xdg.configFile."kitty/kitty.conf" = ./kitty.conf;
-  xdg.configFile."kitty/OceanicMaterial.conf" = ./OceanicMaterial.conf;
+  xdg.configFile."kitty/kitty.conf".source = ./kitty.conf;
+  xdg.configFile."kitty/OceanicMaterial.conf".source = ./OceanicMaterial.conf;
 }
