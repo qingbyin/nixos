780cb510f8089ca758035ffa9ae1ccc295f8fd45
Qing Yin <qingbyin@gmail.com>
2024-02-24 23:34:46 +0800

    Remove vim fold plugin

--- modules/neovim/neovim.nix
+++ modules/neovim/neovim.nix
@@ -90,29 +90,6 @@ in
             autocmd FileType cpp setlocal commentstring=//\ %s
         '';
       }
-      promise-async # nvim-ufo deps
-      {
-        plugin = nvim-ufo; #  folded lines like other modern
-        type = "lua";
-        config = ''
-            vim.o.foldcolumn = '1' -- '0' is not bad
-            vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
-            vim.o.foldlevelstart = 99
-            vim.o.foldenable = true
-            vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
-
-            -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
-            vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
-            vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
-
-            -- use treesitter as ufo main provider
-            require('ufo').setup({
-                    provider_selector = function(bufnr, filetype, buftype)
-                    return {'treesitter', 'indent'}
-                    end
-            })
-        '';
-      }
       vim-smoothie # Smooth scroll
       vim-lastplace # Reopen files at the last edit position
       vim-toml
