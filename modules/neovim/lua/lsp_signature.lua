84927e7f130d6d4295f7e4f340cbfda225fdd764
Qing Yin <qingbyin@gmail.com>
2024-03-01 23:36:58 +0800

    remove vim lsp signature window

--- modules/neovim/lua/lsp_signature.lua
+++ modules/neovim/lua/lsp_signature.lua
@@ -1,6 +1,6 @@
 local cfg = {
     hint_enable = true, -- virtual hint enable
     hint_prefix = "🐼 ",  -- Panda for parameter, NOTE: for the terminal not support emoji, might crash
-
+    floating_window = false,
 }
 require "lsp_signature".setup(cfg)
