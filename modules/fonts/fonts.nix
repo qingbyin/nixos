816ed4884815022b1f8aef3c806cb320eb5add7e
Qing Yin <qingbyin@gmail.com>
2024-03-02 10:47:40 +0800

    Modify font config

--- modules/fonts/fonts.nix
+++ modules/fonts/fonts.nix
@@ -1,6 +1,8 @@
 { config, pkgs, ... }:
 
 {
+  # Make nix installed fonts available to the system by creating
+  # `.config/fontconfig/config.d/10-hm-fonts.conf` file.
   fonts.fontconfig.enable = true;
   home.packages = with pkgs; [
     # Fonts
@@ -12,8 +14,11 @@
     font-awesome
     corefonts # MS (e.g. Time New Roman)
     (nerdfonts.override { fonts = [ "FiraCode" ]; })# Nerdfont Icons override
+    noto-fonts-color-emoji
   ];
 
+  # Set alias for san-serif, serif and monospace
+  # i.e. set the default font for each category
   xdg.configFile."fontconfig/fonts.conf".text =
   ''
   <?xml version="1.0"?>
@@ -22,32 +27,41 @@
     <alias>
       <family>sans-serif</family>
       <prefer>
-        <family>Noto Sans CJK SC</family>
-        <family>Noto Sans CJK TC</family>
-        <family>Noto Sans CJK HK</family>
-        <family>Noto Sans CJK JP</family>
-        <family>Noto Sans CJK KR</family>
-        <family>Lohit Devanagari</family>
+        <family>Source Han Sans</family>
+        <family>FiraCode Nerd Font</family>
       </prefer>
     </alias>
     <alias>
       <family>serif</family>
       <prefer>
-        <family>Noto Serif CJK SC</family>
-        <family>Noto Serif CJK TC</family>
-        <family>Noto Serif CJK JP</family>
-        <family>Noto Serif CJK KR</family>
-        <family>Lohit Devanagari</family>
+        <family>Source Han Serif</family>
+        <family>Times New Roman</family>
       </prefer>
     </alias>
     <alias>
       <family>monospace</family>
       <prefer>
+        <family>FiraCode Nerd Font Mono</family>
         <family>Noto Sans Mono CJK SC</family>
         <family>Noto Sans Mono CJK TC</family>
-        <family>Noto Sans Mono CJK HK</family>
-        <family>Noto Sans Mono CJK JP</family>
-        <family>Noto Sans Mono CJK KR</family>
+      </prefer>
+    </alias>
+  </fontconfig>
+  '';
+
+  # Create my own font combination
+  # ref:https://github.com/alacritty/alacritty/issues/957
+  xdg.configFile."fontconfig/conf.d/99-alias-fonts.conf".text = ''
+  <?xml version="1.0"?>
+  <!DOCTYPE fontconfig SYSTEM "urn:fontconfig:fonts.dtd">
+  <fontconfig>
+    <alias>
+      <family>FiraCode SourceHanSerif</family>
+      <prefer>
+          <family>FiraCode Nerd Font</family>
+          <family>Source Han Serif</family>
+          <family>Noto Color Emoji</family>
+          <family>Unifont</family>
       </prefer>
     </alias>
   </fontconfig>
