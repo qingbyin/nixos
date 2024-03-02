479c893c291604e4f37495faf54ecee5adde57b7
Qing Yin <qingbyin@gmail.com>
2024-03-02 09:22:49 +0800

    fix toggleterm keymaps

--- modules/neovim/lua/toggleterm.lua
+++ modules/neovim/lua/toggleterm.lua
@@ -28,8 +28,8 @@ end
 -- Terminal window mappings
 function _G.set_terminal_keymaps()
   local opts = {buffer = 0}
-  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
-  -- vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+  -- vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts) -- bug: cann not exit in lazygit commit text
+  vim.keymap.set('t', '<C-n>', [[<C-\><C-n>]], opts)
 end
 -- if you only want these mappings for toggle term use term://*toggleterm#* instead
 vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
