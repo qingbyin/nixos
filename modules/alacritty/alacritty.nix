4deaa070b76631ac0933327ee4cf7bdf6c0d6535
Qing Yin <qingbyin@gmail.com>
2024-04-23 16:34:33 +0800

    update alacritty config

--- modules/alacritty/alacritty.nix
+++ modules/alacritty/alacritty.nix
@@ -1,6 +1,6 @@
 { config, pkgs, ... }:
 
 {
-  xdg.configFile."alacritty/alacritty.yml".source = ./alacritty.yml;
+  xdg.configFile."alacritty/alacritty.toml".source = ./alacritty.toml;
   xdg.configFile."tmux/tmux.conf".source = ./tmux.conf;
 }
