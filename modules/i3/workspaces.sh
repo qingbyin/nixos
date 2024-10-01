ffa5e1446eefa75e796ae566c001e2cf1ebc27ad
Qing Yin <qingbyin@gmail.com>
2024-10-01 21:01:27 +0800

    add chatgpt workspace

--- modules/i3/workspaces.sh
+++ modules/i3/workspaces.sh
@@ -7,7 +7,8 @@ i3-msg -t subscribe -m '["workspace", "output"]' | {
         name: name,
         output: "eDP-1-1",
     };
-    . + [fake_ws("1") , fake_ws("2"),
+    . + [fake_ws("1"), fake_ws("2"),
+         fake_ws("a"),
          fake_ws("p🗎"), fake_ws("n󰆧"),
          fake_ws("i"), fake_ws("m"),
          fake_ws("u"), fake_ws("r"), fake_ws("t")] | unique_by(.name)
